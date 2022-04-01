d79c7f6493b991025794397f5e1dddf12a3cc46a
tnguy753 <nguyenthao989@gmail.com>
2022-04-02 00:13:23 +0700

    UI Fix for homepage and footer

--- header.js
+++ header.js
@@ -13,28 +13,13 @@ let appHeader = `
                       </a>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="#"></a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="#">Pricing</a>
-                  </li>
-                  <li class="nav-item">
                       <a class="nav-link" href="#">About</a>
                   </li>
-                  <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                          <div class="dropdown-menu">
-                              <a class="dropdown-item" href="#">Action</a>
-                              <a class="dropdown-item" href="#">Another action</a>
-                              <a class="dropdown-item" href="#">Something else here</a>
-                          <div class="dropdown-divider"></div>
-                              <a class="dropdown-item" href="#">Separated link</a>
-                          </div>
-                  </li>
               </ul>
-              <a href="/user/register.html">
-                  <button class="btn btn-danger">Register</button>
-              </a>
+              <form class="d-flex">
+                <input class="form-control me-sm-2" type="text" placeholder="Search">
+                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+               </form>
               <a href="/user/signin.html">
                   <button class="btn btn-danger">Sign In</button>
               </a>
