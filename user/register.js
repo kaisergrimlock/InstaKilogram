4e0eedd94c39b4f86a070ce162dd308f644fffd1
kaisergrimlock <s3891987@rmit.edu.vn>
2022-04-05 09:12:35 +0700

    Update register.js
    Use some new regex

--- user/register.js
+++ user/register.js
@@ -3,17 +3,21 @@ var lname = document.getElementById('lname')
 var password = document.getElementById('password')
 var retype = document.getElementById('retype-password')
 var email = document.getElementById('email')
+pat = //
 
 function validateForm(){
+    let upper = /[A-Z]/
+    let lower = /[a-z]/
+    let num = /[0-9]/
     if(password.value.length < 8 || password.value.length > 20){
         alert('please enter password between 8 and 20')
     }
 
-    if (isUpper(password.value) != true){
+    if(upper.test(password.value) != true){
         alert('please use at least one upper case')
     }
 
-    if (isLower(password.value) != true){
+    if (lower.test(password.value) != true){
         alert('please use at least one lower case')
     }
 
@@ -21,32 +25,13 @@ function validateForm(){
         alert('please retype the same password')
     }  
     
-    if (isLower(password.value) == true){
+    if (isSpace(password.value) == true){
         alert('please do not use space')
     }
-}
 
-function isUpper(str) {
-    var i = 0
-    for (let i=0; i < str.length; i++)
+    if(num.test(password.value) != true)
     {
-        let character = str.charAt(i)
-        if (character === character.toUpperCase())
-        {
-            return true
-        }
-    }
-}
-
-function isLower(str) {
-    var test = 0
-    for (let i=0; i < str.length; i++)
-    {
-        let character = str.charAt(i)
-        if (character === character.toLowerCase())
-        {
-            return true
-        }
+        alert('please use at least one number')
     }
 }
 
