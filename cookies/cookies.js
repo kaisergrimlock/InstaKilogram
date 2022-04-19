5d0c5f5cdc0126ff2a1ad77cdf32a8c896bf5b8e
kaisergrimlock <s3891987@rmit.edu.vn>
2022-04-19 18:09:00 +0700

    update cookies

--- cookies/cookies.js
+++ cookies/cookies.js
@@ -1,6 +1,6 @@
-setCookies = (cName, cValue, expDate) => {
+setCookies = (cName, cValue, expDays) => {
     let date = new Date();
-    date.setTime(date.getTime() + (expDate * 24 * 60 * 60 * 100))
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 100))
     const expires = "expires = " + date.toUTCString();
     document.cookie = cName + "=" + cValue + ";" + expires + "; path =/";
 
