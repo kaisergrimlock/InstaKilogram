bd0a77b463496bdeba7628d5a05b87f240ca8414
tnguy753 <nguyenthao989@gmail.com>
2022-04-20 14:21:10 +0700

    Fix Toggle Button on Header

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ let appHeader = `
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 <div class="container-fluid">
     <img src="../images/logo1.jpeg" style="width:100px">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
       </button>
           <div class="collapse navbar-collapse" id="navbarColor02">
