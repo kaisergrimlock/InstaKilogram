8210a5cd98be6d7be71274f63bac4a293c867ccb
kaisergrimlock <s3891987@rmit.edu.vn>
2022-04-30 15:24:48 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,46 +1,25 @@
-<!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="utf-8">
- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.1.3/dist/litera/bootstrap.min.css" integrity="sha256-oNAzP5red1ldRKkdYgkxkykk2qXm3hcosesq9UGvN4o=" crossorigin="anonymous">
-
-<!-- Load an icon library -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<footer class="mt-auto">
-    <section class="footer">
-        <div class="social list-group-item list-group-item-action flex-column align-items-start">
-          <a href="#"><i class="fab fa-instagram"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
-          <a href="#"><i class="fab fa-facebook-f"></i></a>
-        </div>
-        <div class="list-group-item list-group-item-action flex-column align-items-start">
-            <ul class="list list-unstyled">
-            <li>
-                <a href="#">About</a>
-            </li>  
-            <li>
-                <a href="#">Privacy</a>
-            </li>
-            <li>
-                <a href="#">Help Links</a>
-            </li>    
-            </ul> 
-            <p class="copyright text-muted">
-                Copyright &copy; Group1. All Rights Reserved
-            </p>
-        </div>
+<footer class="bg-dark text-center text-white">
+  <div class="container">
+    <section class="mb-4 p-4">
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
     </section>
-  </footer>
+    <section class="links">
+      <div class="row">
+          <div class="col-sm">
+            <a href="#" class="text-white">Privacy</a>
+          </div>
+          <div class="col-sm">
+            <a href="#" class="text-white">About</a>
+          </div>
+          <div class="col-sm">
+            <a href="#" class="text-white">Help</a>
+          </div>
+      </div>
+    </section>
+  </div>
+  <div class="text-center p-3">
+    <p>Copyright &copy; Group1. All Rights Reserved</p>
+  </div>
+</footer>
