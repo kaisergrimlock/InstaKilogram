0b5ddf592bd6cb282ef149bd906c9b4403392ff1
tnguy753 <nguyenthao989@gmail.com>
2022-05-03 02:02:36 +0700

    NewFeed UI Fix

--- user/script.php
+++ user/script.php
@@ -98,11 +98,15 @@ function post_image_feed(){
         $row = 1;
         while (($data =  fgetcsv($handle,1000,",")) !== FALSE) {
                 if ($data[2] == 'public'){
-                    echo '<div class="card mb-3 feed-post">';
-                    echo '<p class="card-header"> Posted by '.$data[3].'</p>';
-                    echo '<img src="./img_post/'.$data[1].'" alt="post images" >';
-                    echo '<p class="card-text">'.$data[0].'</p>';
-                    echo '</div>';
+            echo '<div class="card mb-3">';
+            echo '<h3 class="card-header">'.$data[0].'</h3>';
+            echo '<div class="card-body">';
+            echo '<h6 class="card-title text-info">' .$data[3]. '</h6>';
+            echo '<p class="card-subtitle text-muted"> '.$data[4].'</>';
+            echo '<h6> <em>'.$data[2].'</em> </h6>';
+            echo '</div>';
+            echo '<img src="./img_post/'.$data[1].'" width="40%"></img>';
+            echo '</div> ';
                 }
         }
     }else{
