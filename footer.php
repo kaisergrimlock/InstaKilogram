e5c315b8ad5b39a244b150a03b3be1b977444546
kaisergrimlock <s3891987@rmit.edu.vn>
2022-05-03 22:46:12 +0700

    footer update

--- footer.php
+++ footer.php
@@ -1,66 +1,11 @@
-<style>     
-footer{
-  position:absolute;
-  bottom:0;
-  width:100%;
-  height: 60px;
-}
-.card{
-  padding: 2% 7%;
-  color: #646771;
-  background-color: #16151a;
-  width: 100%;
-}
-ul{
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-}
-ul >li{
-  padding: 4px;
-}
- ul > li:hover{
-  color:#957bda;
-  cursor: pointer;
-}
-hr{
-  border-width: 3px;
-}
-        
-        .social > i{
-            padding: 1%;
-            font-size: 15px;
-        }
-        
-        .social > i:hover{
-            color:#957bda;
-            cursor:pointer;
-        }
-        
-        .policy > div{
-            padding: 4px;
-        }
-        
-        .heading{
-            font-family: 'Titillium Web', sans-serif;
-            color: white;
-        }
-        
-        .divider{
-            border-top: 2px solid rgba(189, 196, 203, 0.5);;
-        }
-        
-      
-  </style>
-  <footer>
+<link rel="stylesheet" href="footer.css">
+<footer>
 <div class="bg-dark text-center text-white mt-5">
             <div class="card mx-7 ">
                 <div class="row mb-4 ">
                     <div class="col-md-4 col-sm-11 col-xs-11">
                         <div class="footer-text pull-left">
                              <h1> InstaKilogram</h1>
-                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi non pariatur numquam animi nam at impedit odit nisi.</p>
-
                              <div class="social mt-2 mb-3">
                                 <i class="fa fa-facebook-official fa-lg" ></i>
                                 <i class="fa fa-instagram fa-lg"></i>
@@ -75,41 +20,18 @@ hr{
                     <div class="col-md-2 col-sm-1 col-xs-1 mb-2"></div>
                     
                     <div class="col-md-2 col-sm-4 col-xs-4">
-                 
                         <a href="./aboutus.php"><h5 class="heading">About</h5></a>
-                        <ul >
-                            <li>IT Consulting -</li>
-                            <li>Development</li>
-                            <li>Cloud</li>
-                            <li>DevOps & Support</li>
-                        </ul>
-                 
                     </div>
                  
                  
                     <div class="col-md-2 col-sm-4 col-xs-4">
                  
                     <a href="./privacy.php"><h5 class="heading">Privacy</h5></a>
-                        <ul class="card-text">
-                            <li>Finance</li>
-                            <li>Public Sector</li>
-                            <li>Smart Office</li>
-                            <li>Retail</li>
-                        </ul>
-                 
                     </div>
 
                  
                      <div class="col-md-2 col-sm-4 col-xs-4">
-
                      <a href="./help.php"><h5 class="heading">Help</h5></a>
-                        <ul class="card-text">
-                            <li>About Us</li>
-                            <li>Blog</li>
-                            <li>Contact</li>
-                            <li>Join Us</li>
-                        </ul>
-
                      </div>
 
                 </div>
@@ -117,8 +39,6 @@ hr{
                 
                 <div class="divider mb-4" >   
                 </div>
-    
-               
                 <div class="row" style="font-size:10px;">
                 
                 <div class="col-md-6 col-sm-6 col-xs-6">
@@ -143,15 +63,8 @@ hr{
                        <div>Cookie Policy</div>
                         
                     </div>
-                    
-                    
-                    
                 </div>
-                 
-                 
-                 
              </div>
-            </div>
-
         </div>
-      </footer>
+    </div>
+</footer>
