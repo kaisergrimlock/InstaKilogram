34cd2ee0ca1f9d1d76bccde082b44b43a5bd6576
tnguy753 <nguyenthao989@gmail.com>
2022-05-05 18:55:06 +0700

    Client  Form  Validate

--- user/register.js
+++ user/register.js
@@ -13,7 +13,6 @@ function validateForm(){
         alert('please enter password between 8 and 20')
     }
 
-<<<<<<< Updated upstream
     if(upper.test(password.value) != true){
         alert('please use at least one upper case')
     }
@@ -22,60 +21,52 @@ function validateForm(){
         alert('please use at least one lower case')
     }
 
-=======
-function validateForm(){
-    let upper = /[A-Z]/
-    let lower = /[a-z]/
-    let num = /[0-9]/
-    if(password.value.length < 8 || password.value.length > 20){
-        alert('please enter password between 8 and 20')
-    }
+// function validateForm(){
+//     let upper = /[A-Z]/
+//     let lower = /[a-z]/
+//     let num = /[0-9]/
+//     if(password.value.length < 8 || password.value.length > 20){
+//         alert('please enter password between 8 and 20')
+//     }
 
-    if(upper.test(password.value) != true){
-        alert('please use at least one upper case')
-    }
+//     if(upper.test(password.value) != true){
+//         alert('please use at least one upper case')
+//     }
 
-    if (lower.test(password.value) != true){
-        alert('please use at least one lower case')
-    }
+//     if (lower.test(password.value) != true){
+//         alert('please use at least one lower case')
+//     }
 
->>>>>>> Stashed changes
-    if (password.value != retype.value){
-        alert('please retype the same password')
-    }  
+//     if (password.value != retype.value){
+//         alert('please retype the same password')
+//     }  
     
-    if (isSpace(password.value) == true){
-        alert('please do not use space')
-    }
+//     if (isSpace(password.value) == true){
+//         alert('please do not use space')
+//     }
 
-    if(num.test(password.value) != true)
-    {
-        alert('please use at least one number')
-    }
+//     if(num.test(password.value) != true)
+//     {
+//         alert('please use at least one number')
+//     }
 
-    if(!isSpace)
-    {
-        alert('please do not use space')
-    }
-}
+//     if(!isSpace)
+//     {
+//         alert('please do not use space')
+//     }
+// }
 
-function isSpace(str) {
-<<<<<<< Updated upstream
-=======
-    var test = 0
->>>>>>> Stashed changes
-    for (let i=0; i < str.length; i++)
-    {
-        let character = str.charAt(i)
-        if (character === ' ')
-        {
-            return true
-        }
-<<<<<<< Updated upstream
-        else{
-            return false;
-        }
-=======
->>>>>>> Stashed changes
-    }
+// function isSpace(str) {
+//     var test = 0
+//     for (let i=0; i < str.length; i++)
+//     {
+//         let character = str.charAt(i)
+//         if (character === ' ')
+//         {
+//             return true
+//         }
+//         else{
+//             return false;
+//         }
+//     }
 }
