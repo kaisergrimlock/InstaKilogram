3886d707df2e058bd1fe1182eb6d391b679acb44
Khai Hoang <88621191+s3911417@users.noreply.github.com>
2022-05-07 15:48:37 +0700

    update footer pages

--- aboutus.php
+++ aboutus.php
@@ -3,21 +3,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us</title>
+    <link rel="stylesheet" href="/InstaKilogram/aboutus.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
+    <title>About Us</title>
+
 
 </head>
-<body>
+<body class="bg">
     <?php include("bootstrap.php");?>
 
-    <div class="container">   
-        <h3>About Us</h3>
+    <div class="container">
+        <div class="title">
+            <h3>About Us</h3>
+        </div>   
+        <div class="content">
             <p>InstaKilogram is photo sharing socail networking services similar to Instagram.<br> 
                 People can upload photos or videos to our service and share them with other users.<br> 
                 They can also view posts shared by their friends on InstaKilogram.<br>
                 Anyone can create an account by registering an email address and selecting a username.
             </p>
+        </div>    
+        
     </div>
 </body>
 </html>
--- privacy.php
+++ privacy.php
@@ -3,10 +3,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/InstaKilogram/aboutus.css">
     <title>Privacy</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body>
+<body class="bg">
 <?php include("bootstrap.php");?>
 <div class="container">
     <h1>InstaKilogram Data Policy</h1>
