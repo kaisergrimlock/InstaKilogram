a81fe73bff3a4bbeb2ada5e8b2eef39a94cba50b
Khai Hoang <88621191+s3911417@users.noreply.github.com>
2022-05-08 16:52:50 +0700

    update footer

--- aboutus.php
+++ aboutus.php
@@ -1,29 +1,41 @@
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/InstaKilogram/aboutus.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>About Us</title>
-
-
-</head>
+	<title>About Us Section</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+	<link rel="stylesheet" href="aboutus.css">
+</head>	
 <body class="bg">
     <?php include("bootstrap.php");?>
+	<div class="section">
+		<div class="container">
+			<div class="content-section">
+				<div class="title">
+					<h1>About Us</h1>
+				</div>
+				<div class="content">
+					<h3>InstaKilogram is photo sharing socail networking services similar to Instagram.</h3>
+					<p>People can upload photos or videos to our service and share them with other users.<br> 
+                    They can also view posts shared by their friends on InstaKilogram.<br>
+                    Anyone can create an account by registering an email address and selecting a username.</p>
+					<div class="button">
+						<a href="">Read More</a>
+					</div>
+				</div>
+				<div class="social">
+					<a href=""><i class="fab fa-facebook-f"></i></a>
+					<a href=""><i class="fab fa-twitter"></i></a>
+					<a href=""><i class="fab fa-instagram"></i></a>
+				</div>
+			</div>
+			<div class="image-section">
+				<img src="./images/logo.jpeg">
+			</div>
+		</div>
+	</div>
 
-    <div class="container">
-        <div class="title">
-            <h3>About Us</h3>
-        </div>   
-        <div class="content">
-            <p>InstaKilogram is photo sharing socail networking services similar to Instagram.<br> 
-                People can upload photos or videos to our service and share them with other users.<br> 
-                They can also view posts shared by their friends on InstaKilogram.<br>
-                Anyone can create an account by registering an email address and selecting a username.
-            </p>
-        </div>    
-        
-    </div>
+	
 </body>
 </html>
--- help.php
+++ help.php
@@ -3,16 +3,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/InstaKilogram/help.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Help</title>
+    <link rel="stylesheet" href="help.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+    <title>Help Links</title>
 </head>
-<body>
-    <?php include("bootstrap.php")?>
+<body class="bg">
+    <?php include("bootstrap.php");?>
     <section class="contact">
         <div class="content">
-            <h2>Welcome! How can we help?</h2>
-            <p>You can contact us directly via contact information below. We will reponse within 24 hours.</p>
+            <h2>Contact Us</h2>
+            <p>You can contact us directly via information below. We will reponse within 24 hours</p>
         </div>
         <div class="container">
             <div class="contactInfo">
@@ -20,18 +20,46 @@
                     <div class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
                     <div class="text">
                         <h3>Address</h3>
-                        <p>702 Nguyen Van Linh District 7, Ho Chi Minh City</p>
+                        <p>702 Nguyen Van Linh District 7,<br> Ho Chi Minh City</p>
                     </div>
-                </div> 
+                </div>
                 <div class="box">
-                    <div class="icon"><i class="fa fa-envelope" aria-hidden="true"></i></div>
+                    <div class="icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
+                    <div class="text">
+                        <h3>Phone</h3>
+                        <p>0911261439</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
                     <div class="text">
                         <h3>Email</h3>
-                        <p>s3911417@rmit.edu.vn</p>
+                        <p>InstaKilogram@gmail.com</p>
                     </div>
-                </div>  
-            </div> 
+                </div>
+            </div>
+            <div class="contactForm">
+                <form>
+                    <h2>Send Message</h2>
+                    <div class="inputBox">
+                        <input type="text" name="" require="required">
+                        <span>Full name</span>
+                    </div>
+                    <div class="inputBox">
+                        <input type="text" name="" require="required">
+                        <span>Email</span>
+                    </div>
+                    <div class="inputBox">
+                        <textarea require="required"></textarea>
+                        <span>Type your Message</span>
+                    </div>
+                    <div class="inputBox">
+                        <input type="submit" name="" value="Send">
+                    </div>
+                </form>
+            </div>
         </div>
     </section>
+    
 </body>
 </html>
