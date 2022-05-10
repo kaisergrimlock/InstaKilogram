cd3f404bc823cd6de310d828e3b7ff4843b546b9
kaisergrimlock <s3891987@rmit.edu.vn>
2022-05-10 19:20:01 +0700

    Revert "Merge branch 'main' of https://github.com/kaisergrimlock/Full-Stacked-Website"
    This reverts commit bf89261e386edaacc004c643b9a97e1787470a6c, reversing
    changes made to ce4010ccf974b1c978db40abc6e2028609d2cedc.

--- user/script.php
+++ user/script.php
@@ -24,12 +24,14 @@ if(isset($_POST["btn_signup"])){
     $specialChars = preg_match('@[^\w]@', $password);
    
     if(strlen($password) < 8 || !$number || !$uppercase || !$lowercase || !$specialChars) {
+      $msg = "Password must be at least 8 characters in length and must contain at least one number, one upper case letter, one lower case letter and one special character.";
       return false;
     } else {
-        return true;
+      $msg = "Your password is strong.";
+      
     }
     if ($_POST["password"] === $_POST["reptpassword"]) {
-        return true;
+        // success!
      }
      else {
         return false;
@@ -115,27 +117,21 @@ if(isset($_POST["btn_signup"])){
     }
 
 #Replace Profile Image
-if(isset($_POST["replace_submit"])){
-    $img = " ";
+function replace_profile_img(){
     $email = $_SESSION['email'];
-    if (($handle = fopen('../user/account.csv','r'))!== FALSE){
+    if (($handle = fopen('../user/user_data.csv','r'))!== FALSE){
         while (($data =  fgetcsv($handle,1000,",")) !== FALSE) {
             if ($data[1] == $email){
                     $img = $data[6];
-                    
             }
         }
     }
-    if(file_exists($img)){
-        unlink($filename);
-    }
-    $img_name=$_FILES['replace_image']['name'];
-	$tmp_img_name=$_FILES['replace_image']['tmp_name'];
+    $img_name=$_FILES['profile_change']['name'];
+	$tmp_img_name=$_FILES['profile_change']['tmp_name'];
     $img_name = $img;
     $folder='profile_img/';
 	move_uploaded_file($tmp_img_name,$folder.$img_name);
 }
-
 
 
 #Post Image & Message Function
