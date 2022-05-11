459cb2748f7ff998625a84485e0da67a8a9fbf38
kaisergrimlock <s3891987@rmit.edu.vn>
2022-05-11 22:14:54 +0700

    Update crud.php

--- admin/crud.php
+++ admin/crud.php
@@ -6,29 +6,38 @@ if(!isset($_SESSION['admin'])){
     header('location: signin.php');
 }else{
 ?>
-
-<!-- DISPLAY USERS -->
-<br>
-<div>
-    <table class="table table-hover text-center">
-        <thead>
-            <tr class="table-dark">
-                <th scope="col">No</th>
-                <th scope="col">Email</th>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
-                <th scope="col">RegisterDate</th>
-                <th scope="col">Details</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php display_table()?>
-        </tbody>
-    </table>
-</div>
-</section>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <br>
+        <div>
+            <!-- DISPLAY USERS -->
+            <table class="table table-hover text-center">
+                <thead>
+                    <tr class="table-dark">
+                        <th scope="col">No</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">RegisterDate</th>
+                        <th scope="col">Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php display_table()?>
+                </tbody>
+            </table>
+        </div>
+    </section>
 </body>
-
+</html>
 </html>
 <?php
 }?>
