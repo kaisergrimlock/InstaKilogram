9f62f986d1f11625d201cafc2e65ffab3637fe58
kaisergrimlock <s3891987@rmit.edu.vn>
2022-05-13 14:34:52 +0700

    w3c update

--- admin/post.php
+++ admin/post.php
@@ -6,16 +6,6 @@ if(!isset($_SESSION['admin'])){
     header('location: signin.php');
 }else{
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>List of Users</title>
-</head>
-
 <body>
     <main>
         <table class="table table-hover text-center">
