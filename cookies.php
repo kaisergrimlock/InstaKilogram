c4385a646b6302942a3060b61416118c82e2329e
kaisergrimlock <s3891987@rmit.edu.vn>
2022-05-13 15:08:22 +0700

    Update cookies.php

--- cookies.php
+++ cookies.php
@@ -1,26 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <title>Cookies</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="cookies.js" defer></script>
-</head>
-
-<body>
-    <div id="cookies">
-        <div class="alert alert-dismissible alert-light">
-            <div class="cookies">
-                <p>We use cookies to personalize your experience. By continuing to this website you agree to our use of
-                    cookies</p>
-                <button id="cookies-btn" type="button" class="btn btn-info">Got it!</button>
-                <a href="/InstaKilogram/home/privacy.php"> Read our policies
-                </a>
-            </div>
+<div id="cookies">
+    <div class="alert alert-dismissible alert-light">
+        <div class="cookies">
+            <p>We use cookies to personalize your experience. By continuing to this website you agree to our use of
+                cookies</p>
+            <button id="cookies-btn" type="button" class="btn btn-info">Got it!</button>
+            <a href="/InstaKilogram/home/privacy.php"> Read our policies
+            </a>
         </div>
     </div>
-</body>
-
-</html>
+</div>
