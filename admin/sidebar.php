7fc213d82d35875aa466b2fd1eadd8c2b2ef6dca
kaisergrimlock <s3891987@rmit.edu.vn>
2022-05-13 15:17:33 +0700

    Update sidebar.php

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -16,13 +16,7 @@
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-    <script>
-        $(document).ready(function () {
-            $('#sidebarCollapse').on('click', function () {
-            $('#sidebar').toggleClass('active');
-    });
-    });
-    </script>
+    <script src="sidebar.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
