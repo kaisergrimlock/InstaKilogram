cf63c580ba1a62d606b42f3502c7207d2d03402f
kaisergrimlock <s3891987@rmit.edu.vn>
2022-05-13 14:57:45 +0700

    update search.php w3c

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -16,10 +16,10 @@
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-    <script type="text/JavaScript">
+    <script>
         $(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
+            $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
     });
     });
     </script>
